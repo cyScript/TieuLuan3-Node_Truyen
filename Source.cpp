a0981a9c4c98212842e54190fe21e548282bca48
cyScript <tienphat3968@gmail.com>
2021-10-02 15:53:22 +0700

    Add features

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define TL3 "D:\\QuanLySach.txt"
+#define MAX 100
+
+typedef struct Date
+{
+	int Ngay;
+	int Thang;
+	int Nam;
+};
 typedef struct QLSach
 {
 	char MaSach[21];
@@ -8,99 +17,215 @@ typedef struct QLSach
 	char TheLoai[16];
 	int NamXuatBan;
 	int GiaBan;
-}Sach;
+	Date NgayThue;
+	Date NgayTra;
+} Sach;
 
+void NhapDate(Date &d)
+{
+	// int ngay, thang, nam;
+	printf("Nhap ngay,thang,nam: ");
+	scanf("%d%d%d", &d.Ngay, &d.Thang, &d.Nam);
+	switch (d.Thang)
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+
+		if (d.Ngay > 0 && d.Ngay <= 31)
+		{
+
+			printf("Ngay %d thang %d nam %d hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+
+		else
+
+		{
+			printf("Ngay %d thang %d nam %d ko hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		if (d.Ngay <= 30 && d.Ngay > 0)
+		{
+			printf("Ngay %d thang %d nam %d hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+		else
+		{
+			printf("Ngay %d thang %d nam %d ko hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+	case 2:
+		if (d.Ngay <= 28 && d.Ngay > 0)
+		{
+			printf("Ngay %d thang %d nam %d hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+		else
+		{
+			printf("Ngay %d thang %d nam %d ko hop le\n", d.Ngay, d.Thang, d.Nam);
+			break;
+		}
+	default:
+		printf("Ngay %d thang %d nam ko hop le\n", d.Ngay, d.Thang, d.Nam);
+		break;
+	}
+	// if (d.Nam % 400 == 0)
+	// 	printf("Nam %d la nam nhuan", d.Nam);
+	// else
+	// {
+	// 	if (d.Nam % 4 == 0 && d.Nam % 100 != 0)
+	// 		printf("Nam %d la nam nhuan", d.Nam);
+	// 	else
+	// 		printf("Nam %d ko phai la nam nhuan", d.Nam);
+	// }
+}
 
 void Nhap(Sach &s)
 {
-	printf("\nNhap ma sach: ");
+	fflush(stdin);
+	printf("Nhap ma sach: ");
 	gets(s.MaSach);
-	gets(s.MaSach);
-	printf("\nNhap ten sach: ");
+	printf("Nhap ten sach: ");
 	gets(s.TenSach);
-	printf("\nNhap ten tac gia: ");
+	printf("Nhap ten tac gia: ");
 	gets(s.TacGia);
-	printf("\nNhap the loai sach: ");
+	printf("Nhap the loai sach: ");
 	gets(s.TheLoai);
-	printf("\nNhap nam xuat ban: ");
-	scanf("%d",&s.NamXuatBan);
-	printf("\nNhap gia ban: ");
-	scanf("%d",&s.GiaBan);
+	printf("Nhap nam xuat ban: ");
+	scanf("%d", &s.NamXuatBan);
+	printf("Nhap gia ban: ");
+	scanf("%d", &s.GiaBan);
+	printf("Nhap ngay thue:\n");
+	NhapDate(s.NgayThue);
+	printf("\nNhap ngay tra:\n");
+	NhapDate(s.NgayTra);
 }
 void Xuat(Sach &s)
 {
-	printf("\nMa sach: %s",s.MaSach);
-	printf("\nTen sach: %s",s.TenSach);
-	printf("\nTen tac gia: %s",s.TacGia);
-	printf("\nThe loai sach: %s",s.TheLoai);
-	printf("\nNam xuat ban: %d",s.NamXuatBan);
-	printf("\nGia ban: %d",s.GiaBan);
+	printf("\nMa sach: %s", s.MaSach);
+	printf("\nTen sach: %s", s.TenSach);
+	printf("\nTen tac gia: %s", s.TacGia);
+	printf("\nThe loai sach: %s", s.TheLoai);
+	printf("\nNam xuat ban: %d", s.NamXuatBan);
+	printf("\nGia ban: %d", s.GiaBan);
 }
-typedef struct Stack{
-	Sach info;
-	struct Stack* next;
-}Stack;
- 
-//Khoi tao ngan xep
-void Init (Stack &S){
-   S = NULL;
-}
-//Kiem tra ngan xep rong
-int Isempty(Stack S){
-    return S==NULL;
-}
-
-Stack CreateNode(int x){
-	Stack p=new Node;
-	p->info = x;
-	p->next = NULL;
-	return p;
-}
-void InsertFirst(Stack &S, Stack pNew)
+void NhapDs(Sach s[], int n)
 {
-	if(Isempty(S))
-		S = pNew;
-	else
+	for (int i = 0; i < n; i++)
 	{
-		pNew->next = S;
-		S = pNew;
+		printf("Nhap thong tin quyen sach thu %d: \n", i + 1);
+		Nhap(s[i]);
 	}
 }
-
-// Them du lieu ngan xep
-bool Push(Stack &S, int newItem)
+void XuatDs(Sach s[], int n)
 {
-	Stack pNew = CreateNode(newItem);
-	InsertFirst(S,pNew);
+	for (int i = 0; i < n; i++)
+		Xuat(s[i]);
 }
-
-// Lay du lieu ra khoi ngan xep
-bool Pop(Stack &S, int &outItem)
+struct Node
 {
-	if(s==NULL) return false;
-	Stack pTop = S;
-	S = S->next;
-	outItem = pTop->info;
-	delete pTop;
-	return true;
-}
+	Sach info;
+	Node *next;
+};
+typedef struct Node *Stack;
+// struct Stack
+// {
+// 	Node *pHead;
+// 	Node *pTail;
+// };
 
+//Khoi tao ngan xep
+// void Init(Stack &s)
+// {
+// 	S = NULL;
+// }
+//Kiem tra ngan xep rong
+// bool IsEmpty(Stack s)
+// {
+// 	return (s == NULL);
+// }
+// Node *CreateNode(Sach info)
+// {
+// 	Node *p = new Node();
+// 	if (p == NULL)
+// 	{
+// 		return NULL;
+// 	}
+// 	p->info = info;
+// 	p->next = NULL;
+// 	return p;
+// }
+// void InsertFirst(Stack &S, Stack pNew)
+// {
+// 	if (IsEmpty(S))
+// 		S = pNew;
+// 	else
+// 	{
+// 		pNew->next = S;
+// 		S = pNew;
+// 	}
+// }
 
+// // Them du lieu ngan xep
+// bool Push(Stack &S, int newItem)
+// {
+// 	Stack pNew = CreateNode(newItem);
+// 	InsertFirst(S, pNew);
+// }
 
-void DocFile(Sach s[]){
-	FILE *f = fopen(TL3, "r");
-	for(int i = 0; i<n; i++)
+// // Lay du lieu ra khoi ngan xep
+// bool Pop(Stack &S, int &outItem)
+// {
+// 	if (s == NULL)
+// 		return false;
+// 	Stack pTop = S;
+// 	S = S->next;
+// 	outItem = pTop->info;
+// 	delete pTop;
+// 	return true;
+// }
+// void DocFile(Sach s[], int n)
+// {
+// 	FILE *f = fopen(TL3, "r");
+// 	if (f == NULL)
+// 		printf("\nDoc file that bai!");
+// 	char tmp[3];
+// 	fscanf(f, "%s", &tmp);
+// 	for (int i = 0; i <= MAX; i++)
+// 	{
+// 		fscanf(f, "%s\t%s\t%s\t\t%s\t%d\t%d", &s[i].MaSach, &s[i].TenSach, &s[i].TacGia, &s[i].TheLoai, &s[i].NamXuatBan, &s[i].GiaBan);
+// 	}
+// 	fclose(f);
+// }
+void GhiFile(Sach s[], int n)
+{
+	FILE *f = fopen(TL3, "w+");
+	if (f == NULL)
+		printf("\nGhi file that bai!");
+	fprintf(f, "MaSach\tTenSach\tTacGia\t\tTheLoai\tNamXB\tGiaBan\n");
+	for (int i = 0; i < n; ++i)
 	{
-		fscanf(f,"%s\t%s\t%s\t\t%s\t%d\t%d",&s[i].MaSach,&s[i].TenSach,&s[i].TacGia,&s[i].TheLoai,&s[i].NamXuatBan,&s[i].GiaBan);
+		fprintf_s(f, "%s\t%s\t%s\t\t%s\t%d\t%d", s[i].MaSach, s[i].TenSach, s[i].TacGia, s[i].TheLoai, s[i].NamXuatBan, s[i].GiaBan);
+		fprintf(f, "\n");
 	}
 	fclose(f);
 }
-void Ghifile(Sach s[], int n){
-		FILE *f=fopen(TL3,"w+");
-		fprintf(f,"\nMaSach\tTenSach\tTacGia\t\tTheLoai\tNamXB\tGiaBan\n");
-	for(int i = 0;i < n;++i)
-	{
-		fprintf(f,"%s\t%s\t%s\t\t%s\t%d\t%d",s[i].MaSach,s[i].TenSach,s[i].TacGia,s[i].TheLoai,s[i].NamXuatBan,s[i].GiaBan);
-		fprintf(f,"\n");
-	}fclose(f);
+int main()
+{
+	Stack *s;
+	Sach x[MAX];
+	int n = 3;
+	NhapDs(x, n);
+	GhiFile(x, n);
+	// DocFile(x, n);
+	XuatDs(x, n);
 }
